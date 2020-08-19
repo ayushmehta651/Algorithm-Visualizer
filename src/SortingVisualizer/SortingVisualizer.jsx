d53bd9a203a4159bfe489297a2e5e7c08520041f
Ayush mehta <ayushmehta651@gmail.com>
2020-08-19 16:45:10 +0530

    Bubble sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Navbar, Button } from 'reactstrap';
 import {getMergeSortAnimations} from '../sortingAlgorithms/mergeSort.js';
 import { getBubbleSortAnimations } from '../sortingAlgorithms/bubbleSort.js';
+import { getInsertionSortAnimations } from '../sortingAlgorithms/insertionSort.js';
 import './SortingVisualizer.css';
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 500;
+const ANIMATION_SPEED_MS = 5;
 
 // Change this value for the number of bars (value) in the array.
-const NUMBER_OF_ARRAY_BARS = 10;
+const NUMBER_OF_ARRAY_BARS = 92;
 
 // This is the main color of the array bars.
 const PRIMARY_COLOR = 'turquoise';
